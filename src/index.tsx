83730d8978ccdb7a529339f4257941b2349de685
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2021-05-29 18:56:53 +0100

    Initial attempt to integrate bridge (but Babel can't build due to @NativeClass)

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { NativeModules } from 'react-native';
-
-type NativescriptRuntimeType = {
-  multiply(a: number, b: number): Promise<number>;
-};
-
-const { NativescriptRuntime } = NativeModules;
-
-export default NativescriptRuntime as NativescriptRuntimeType;
