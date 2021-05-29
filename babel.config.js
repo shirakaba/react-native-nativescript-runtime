6d434f5d61f79f0f8f0f2205d07448ad528bdf6b
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2021-05-29 19:27:45 +0100

    feat: working directory structure for bridge

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    // Added simply because we don't have a babel transform for @NativeClass yet.
+    '@babel/plugin-transform-classes',
+  ],
 };
