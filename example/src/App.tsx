7d81839fd2f1fe36b3f6cbf6c0a2d7b1ecabcd2c
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2021-06-04 22:22:34 +0100

    Import from src rather than build, for now

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import NativescriptRuntime from 'react-native-nativescript-runtime';
+// import NativescriptRuntime from 'react-native-nativescript-runtime/lib/module/plugin/index';
+import NativescriptRuntime from '../../lib/module/plugin/index';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
