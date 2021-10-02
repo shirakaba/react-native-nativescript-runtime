426a72130570c9103ef1d706cb641be0fb5405f0
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2021-10-02 17:45:31 +0100

    chore: remove unnecessary Foundation import

--- cpp/react-native-nativescript-runtime.h
+++ cpp/react-native-nativescript-runtime.h
@@ -2,14 +2,6 @@
 #define REACT_NATIVE_NATIVESCRIPT_RUNTIME_H
 
 #import "../ios/gNativeScriptHandlers.h"
-
-// We could swap out Foundation's NSDictionary with Core Foundation's CFDictionary,
-// (which is compatible with C++, meaning that this wouldn't have to be Obj-C++.
-// However, the APIs are far more awkward (at both points of use – when initialising
-// it natively in AppDelegate.m and when mutating it from NativeScript).
-// It also doesn't decouple us from Apple SDKs (we'll still need to duplicate this
-// logic for Android), so it doesn't bring any real benefit.
-#include <Foundation/Foundation.h>
 #include <jsi/jsilib.h>
 #include <jsi/jsi.h>
 
